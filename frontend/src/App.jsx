0823327eb505736d49bcc924e52cb57a8431baa7
spoiltcheese <30766633+spoiltcheese@users.noreply.github.com>
2025-08-15 03:15:19 +0800

    react biolerplate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { Suspense, useState, useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router";
+import NavBar from "./components/NavBar";
+import Favourites from "./components/Favourites";
+import Login from "./components/Login";
+import Games from "./components/Games";
 
 function App() {
   return (
     <div>
-      <h2>GA SEB</h2>
+      <Suspense fallback={<p>Loading...</p>}>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/games" />} />
+          <Route path="/games" element={<Games />} />
+          <Route path="/favourites" element={<Favourites id={currentList} />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router";
+import PropTypes from "prop-types";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Gamemonger
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Games
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/favourites">
+                Favourites
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
