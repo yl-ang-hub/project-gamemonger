2ad2552ca6e6a8d3994c2027123160589743f248
Yen's MacStudio <yen.compound@gmail.com>
2025-08-20 21:49:02 +0800

    Raw GetApiGames.js added

--- frontend/src/components/Favourites.jsx
+++ frontend/src/components/Favourites.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Favourites = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="container">
+        <div className="container">
+          <div>Gameplay Video</div>
+          <div>Summary Review</div>
+          <div>Genernal Info</div>
+        </div>
+
+        <div className="container">
+          <div>Game intro</div>
+        </div>
+        <div className="container">
+          <div>Review</div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Favourites;
