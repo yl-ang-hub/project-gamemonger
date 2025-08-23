4759c27e212fbd4a3180613bfc8b0f14cf9ece07
yensg <yen.compound@gmail.com>
2025-08-23 23:14:33 +0800

    Updated with UserReviewsSchema

---
+++ backend/src/models/UserReviews.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const UserReviewsSchema = new mongoose.Schema(
+  {
+    // 1 to 5
+    rating: { type: String, required: true },
+    review: { type: String, required: true },
+    // put in authCtx.userId
+    // userId: { type: String, required: true }, // store just id so easier to delete
+    // rawgId: { type: String, required: true },
+    rawgId: { type: mongoose.Schema.Types.ObjectId, ref: "Game" },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { collection: "reviews" }
+);
+
+export default mongoose.model("Reviews", UserReviewsSchema);
