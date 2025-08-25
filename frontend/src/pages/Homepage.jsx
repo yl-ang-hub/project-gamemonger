fe4181140387c3e2838c36348f40ae4e0f8f0a70
spoiltcheese <30766633+spoiltcheese@users.noreply.github.com>
2025-08-25 21:38:13 +0800

    Chunked homepaage into 3 cards per row

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -3,6 +3,8 @@ import NavBar from "../components/NavBar";
 import useFetch from "../hooks/useFetch";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import Games from "../components/Games";
+import { Container, Row, Col } from "react-bootstrap";
+import chunk from "lodash/chunk";
 
 const Homepage = () => {
   const fetchData = useFetch();
@@ -17,23 +19,46 @@ const Homepage = () => {
     queryFn: getGames,
   });
 
+  const items = query.isSuccess ? query.data.results : [];
+  const chunkedItems = chunk(items, 3);
+
   return (
-    <>
-      <div className="container border border-primary">
-        {/* {JSON.stringify(query.data)} */}
-        {query.isSuccess &&
-          query.data.results.map((item) => {
-            return (
-              <Games
-                key={item.id}
-                rawgId={item.id}
-                name={item.name}
-                background_image={item.background_image}
-              />
-            );
-          })}
-      </div>
-    </>
+    <Container>
+      {query.isLoading && <div>Loading...</div>}
+      {query.isSuccess &&
+        chunkedItems.map((chunk, chunkIndex) => (
+          <Row key={chunkIndex}>
+            {chunk.map((item) => (
+              <Col key={item.id} md={4}>
+                <Games
+                  rawgId={item.id}
+                  name={item.name}
+                  background_image={item.background_image}
+                />
+              </Col>
+            ))}
+          </Row>
+        ))}
+    </Container>
   );
+
+  // return (
+  //   <>
+  //     <div className="container border border-primary">
+  //       {/* {JSON.stringify(query.data)} */}
+  //       {query.isSuccess &&
+  //         query.data.results.map((item, index) => {
+  //           return (
+  //             <Games
+  //               key={item.id}
+  //               rawgId={item.id}
+  //               name={item.name}
+  //               background_image={item.background_image}
+  //             />
+  //           );
+  //         })}
+  //     </div>
+  //   </>
+  // );
 };
 export default Homepage;
