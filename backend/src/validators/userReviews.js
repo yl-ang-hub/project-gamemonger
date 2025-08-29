ee671a9f1e87e2e2fd22f868a8654d732b850cbc
yensg <yen.compound@gmail.com>
2025-08-29 12:05:46 +0800

    Edited the remarks

--- backend/src/validators/userReviews.js
+++ backend/src/validators/userReviews.js
@@ -1,8 +1,8 @@
 import { body } from "express-validator";
 
 export const validateUserReviews = [
-  body("review", "rating is required").notEmpty(),
-  body("rawgId", "password is required").notEmpty(),
-  body("userId", "password is required").notEmpty(),
+  body("review", "review is required").notEmpty(),
+  body("rawgId", "rawgId is required").notEmpty(),
+  body("userId", "userId is required").notEmpty(),
   body("rating", "rating must be between").isLength({ min: 1, max: 5 }),
 ];
